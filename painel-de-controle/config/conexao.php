946c3f51fac0d8aa88a10f9329363150607883af
Guilherme Gonçalves <108967854+gsocria@users.noreply.github.com>
2023-06-23 13:34:46 -0300

    Update conexao.php

--- painel-de-controle/config/conexao.php
+++ painel-de-controle/config/conexao.php
@@ -1,7 +1,7 @@
 <?php
 function conectar() {
     try {
-        $conn = new PDO('mysql:host=localhost; charset=utf8mb4; dbname=db_torneio', 'root', '');
+        $conn = new PDO('mysql:host=localhost; charset=utf8mb4; dbname=', '', '');
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         echo "banco esta com problema" . $e->getMessage();
